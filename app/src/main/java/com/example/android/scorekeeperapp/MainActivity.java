3bc7cac622ccf4233d416023f27d0a624e979d89
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2018-06-06 14:56:11 +0200

    Implement counter buttons Java

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.android.scorekeeperapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //    Declare variables
+    int franceGoals = 0;
+    int franceGoalsId = R.id.franceGoals;
+    int franceYellows = 0;
+    int franceYellowsId = R.id.franceYellowCards;
+    int franceReds = 0;
+    int franceRedsId = R.id.franceRedCards;
+
+    int spainGoals = 0;
+    int spainGoalsId = R.id.spainGoals;
+    int spainYellows = 0;
+    int spainYellowsId = R.id.spainYellowCards;
+    int spainReds = 0;
+    int spainRedsId = R.id.spainRedCards;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addFranceGoal(View v) {
+        franceGoals++;
+        displayNumberInId(franceGoals, franceGoalsId);
+    }
+
+    public void addFranceYellow(View v) {
+        franceYellows++;
+        displayNumberInId(franceYellows, franceYellowsId);
+    }
+
+    public void addFranceRed(View v) {
+        franceReds++;
+        displayNumberInId(franceReds, franceRedsId);
+    }
+
+    public void addSpainGoal(View v) {
+        spainGoals++;
+        displayNumberInId(spainGoals, spainGoalsId);
+    }
+
+    public void addSpainYellow(View v) {
+        spainYellows++;
+        displayNumberInId(spainYellows, spainYellowsId);
+    }
+
+    public void addSpainRed(View v) {
+        spainReds++;
+        displayNumberInId(spainReds, spainRedsId);
+    }
+
+    public void displayNumberInId(int aNumber, int anId) {
+        TextView textToUpdate = (TextView) findViewById(anId);
+        textToUpdate.setText(String.valueOf(aNumber));
     }
 }
